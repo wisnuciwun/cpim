b5bf4607c3efa16228f5c08da5c6c93a0ecab866
wisnuciwun <adiwardhanawisnu@gmail.com>
2021-12-05 18:01:19 +0700

    :rocket: ready to online

--- src/container/DefaultHeader/DefaultHeader.js
+++ src/container/DefaultHeader/DefaultHeader.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './style.sass'
+import ico from './favicon.ico'
 
 class DefaultHeader extends Component {
     render() {
         return (
             <div className="header">
-                <h1>{this.props.pagename} page</h1>
+                <div className="d-flex justify-content-between">
+                    <h1>{this.props.pagename} page</h1>
+                    <h1><img src={ico} style={{width: '50px', height: '50px'}}/>&nbsp;<i>CompaControl</i></h1>
+                </div>
                 <hr></hr>
             </div>
         )
